31641ea91851eb4a736df85ed94b937a7958336f
Dorayen <dorayen@gmail.com>
2025-05-17 19:25:31 -0300

    feat: adicionar configoracao H2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO livro (id, titulo) VALUES ('1', 'USA');
+INSERT INTO livro (id, titulo) VALUES ('2', 'France');
+INSERT INTO livro (id, titulo) VALUES ('3', 'Brazil');
+INSERT INTO livro (id, titulo) VALUES ('4', 'Italy');
+INSERT INTO livro (id, titulo) VALUES ('5', 'Canada');
---
+++ src/test/java/com/biblioteca/leituralia/LeituraliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.leituralia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeituraliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
