35a8717087c947ea8d219463fbd9c68eb93478a1
Dorayen <dorayen@gmail.com>
2025-05-17 19:25:06 -0300

    feat: primer commit

---
+++ src/main/java/com/biblioteca/leituralia/LeituraliaApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.leituralia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeituraliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeituraliaApplication.class, args);
+	}
+
+}
